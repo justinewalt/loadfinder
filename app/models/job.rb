733c840e2ca1d87c1fbdd360b016d796b366c97c
justinewalt <justinewalt@gmail.com>
2017-01-09 18:42:35 -0700

    added _form, edit, index, show, and new for bids and jobs views

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,2 +1,4 @@
 class Job < ApplicationRecord
+  belongs_to :users
+  has_many :jobs
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  has_many :jobs
+  has_many :bids
+
   validates_presence_of :terms_accepted
 
   # before_validation :assign_roles
 
-    # def assign_roles
-    #   self.shipper = (haulier_or_shipper == "shipper")
-    #   self.haulier = (haulier_or_shipper == "haulier")
-    # end
 end
